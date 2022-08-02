9e712a4f3b87ce8f94a91d837e9ed59a79f276b3
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-08-02 12:03:19 +0200

    Header components + scss OK

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
-
+import { BiUserCircle, BiSearch } from 'react-icons/bi';
+import { MdFavoriteBorder } from 'react-icons/md';
 import './style.scss';
 
 export default function Header() {
   return (
-    <div>
-      <h1>LeCoinManga</h1>
-    </div>
+    <div className='header'>
+      <h1 className='header__title'>LeCoinManga</h1>
+      <button className='header__annonce'><span>+</span> Déposer une annonce</button>
+
+      <div className='header__search'>
+        <BiSearch className='header__search--item' />
+        <input
+        type="text"
+        placeholder='Rechercher'
+        className='header__input'
+        name='headerInput'
+        />
+      </div>
+
+      <div className='header__icons'>
+        <div className='header__icons__items'>
+          <MdFavoriteBorder className='header__icons__items--item'/>
+          <span className='header__icons__items__span'>Favoris</span>
+        </div>
+
+        <div className='header__icons__items'>
+        {/* Add Link */}
+          <BiUserCircle className='header__icons__items--item' />
+          <span className='header__icons__items__span'>Profil</span>
+        </div>
+      </div>
+  </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // == Import : npm
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 // == Import : local
 // Composants
@@ -8,7 +9,10 @@ import LeCoinManga from 'src/components/LeCoinManga';
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
-const rootReactElement = <LeCoinManga />;
+const rootReactElement = 
+<BrowserRouter>
+<LeCoinManga />
+</BrowserRouter>;
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
