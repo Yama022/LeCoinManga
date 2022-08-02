cbb1e80d286226809bd3f6a6c8fce3c158d29c15
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-08-02 12:06:55 +0200

    Add Mangas component

--- src/components/LeCoinManga/index.js
+++ src/components/LeCoinManga/index.js
@@ -1,12 +1,14 @@
 // == Import
 
 import Header from 'src/components/Header';
+import Mangas from 'src/components/Mangas';
 import './styles.scss';
 
 // == Composant
 const LeCoinManga = () => (
   <div className="lecoinmanga">
     <Header />
+    <Mangas />
   </div>
 );
 
---
+++ src/components/Mangas/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.scss';
+
+export default function Mangas() {
+  return (
+    <div className='mangas'>Mangas</div>
+  );
+}
