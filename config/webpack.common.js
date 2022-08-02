8781c8a66463d1667a71532183d25893c0d2f4d5
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-08-02 10:37:15 +0200

    first commit + create api folder

---
+++ config/webpack.common.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const paths = require('./paths');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv-flow').config( {
+  path: path.join(paths.root)
+});
+
+module.exports = {
+  entry: [
+    // SCSS
+    paths.src + '/styles/index.scss',
+    // JS
+    paths.src + '/index.js',
+  ],
+  output: {
+    path: paths.build,
+    publicPath: '/',
+    filename: 'js/[name].[contenthash].js',
+  },
+  resolve: {
+    alias: {
+      src: paths.src,
+      app: paths.src,
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin( {
+      "process.env": JSON.stringify(dotenv.parsed)
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: paths.static,
+          to: '',
+        }
+      ],
+    }),
+
+    new HtmlWebpackPlugin({
+      favicon: paths.assets + '/favicon.ico',
+      template: paths.assets + '/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      //JS
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+
+      // Fonts
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'fonts/',
+        },
+      },
+
+      // Images
+      {
+        test: /\.(ico|gif|png|jpe?g|webp|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { outputPath: 'images/' },
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <img src={reactLogo} alt="react logo" />
+    <h1>Composant : App</h1>
+  </div>
+);
+
+// == Export
+export default App;
