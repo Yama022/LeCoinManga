ab095de2eb0b4e8479ad2017cf96c8345b751366
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-08-02 10:41:47 +0200

    Replace App name by LeCoinManga on component and global index.js

---
+++ src/components/LeCoinManga/index.js
@@ -0,0 +1,12 @@
+// == Import
+import './styles.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <h1>LeCoinManga</h1>
+  </div>
+);
+
+// == Export
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { render } from 'react-dom';
 
 // == Import : local
 // Composants
-import App from 'src/components/App';
+import LeCoinManga from 'src/components/LeCoinManga';
 
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
-const rootReactElement = <App />;
+const rootReactElement = <LeCoinManga />;
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
