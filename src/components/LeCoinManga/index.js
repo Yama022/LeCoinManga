a25b6fa074418582ae05bb8d8aa8786cf05341a0
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-08-04 16:19:14 +0200

    Add Route profil, annonce, favoris on LeCoinManga component

--- src/components/LeCoinManga/index.js
+++ src/components/LeCoinManga/index.js
@@ -1,6 +1,9 @@
 // == Import
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from 'src/components/Header';
+import Annonce from 'src/components/Header/annonce';
+import Favoris from 'src/components/Header/favoris';
+import Profil from 'src/components/Header/profil';
 import Mangas from 'src/components/Mangas';
 import Description from 'src/components/Mangas/description';
 import './styles.scss';
@@ -15,6 +18,21 @@ export default function LeCoinManga() {
         <Route exact path="/">
           <Header />
           <Mangas />
+        </Route>
+
+        <Route path="/annonce">
+          <Header />
+          <Annonce />
+        </Route>
+
+        <Route path="/favoris">
+          <Header />
+          <Favoris />
+        </Route>
+
+        <Route path="/profil">
+          <Header />
+          <Profil />
         </Route>
 
         <Route path="/manga">
