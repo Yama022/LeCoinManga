e63ec9c2725f9784fac8be7010c7847710f5df74
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-08-08 10:59:48 +0200

    Add data.json on manga.js

--- src/components/Mangas/index.js
+++ src/components/Mangas/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Manga from './manga';
 
+
 import './style.scss';
 
 export default function Mangas() {
   return (
     <div className='mangas'>
-      <Manga />
-      <Manga />
-      <Manga />
-      <Manga />
-      <Manga />
       <Manga />
     </div>
   );
--- src/components/Mangas/manga.js
+++ src/components/Mangas/manga.js
@@ -9,40 +9,43 @@ import './style.scss';
 export default function Manga() {
   console.log(data);
   return (
+    <>
+    {data.map((manga) => (
     <div className='manga'>
+      
       <NavLink
         to='/manga'
       >
 
-      <div className='manga__item'>
+      <div className='manga__item' key={manga.id}>
 
         <div className='manga__item__image'>
-          <img src={jujutsu} alt='jujutsu' />
+          <img src={manga.image} alt='jujutsu' />
         </div>
 
         <div className='manga__item__content'>
           <div className='manga__item__content__title'>
-            <h1>Jujutsu Kaisen</h1>
+            <h1>{manga.name}</h1>
           </div>
 
           <div className='manga__item__content__origine'>
-            <p>Originie : Japon</p>
+            <p>Originie : {manga.origine}</p>
           </div>
 
           <div className='manga__item__content__author'>
-            <p>Auteur : Akutami Gege</p>
+            <p>Auteur : {manga.author}</p>
           </div>
           
           <div className="manga__item__content__type">
-            <h2>Type: Shonen</h2>
+            <h2>Type: {manga.type}</h2>
           </div>
 
           <div className="manga__item__content__price">
-            <h3>Prix : 6,90€</h3>
+            <h3>Prix : {manga.price}</h3>
           </div>
 
           <div className="manga__item__content__age">
-            <p>Âge conseillé : 12 ans et +</p>
+            <p>Âge conseillé : {manga.age}</p>
           </div>
 
         </div>
@@ -50,5 +53,7 @@ export default function Manga() {
       </div>
       </NavLink>
     </div>
+      ))}
+      </>
   );
 }
