c38c75a5e08aad8892f810fb58d50ce7f3fa88b2
TheoBIET <dev.theobiet@gmail.com>
2022-09-24 15:45:47 +0200

    💄 | Categories Slideshow OK

---
+++ src/components/CategoriesSlider.js
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
+
+export default function CategoriesSlider({ categories }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <ul className="Landing__categories__list">
+      <Slider className="Landing__categories__list" {...settings}>
+        {categories.map((category, index) => (
+          <li key={index} className="Landing__categories__list__item">
+            <Link
+              to={`/category/${category.label}`}
+              className="Landing__categories__list__item__link">
+              <img
+                src={require(`../assets/images/categories/${category.label}.png`)}
+                alt={category.name}
+              />
+              <span>{category.name}</span>
+            </Link>
+          </li>
+        ))}
+      </Slider>
+    </ul>
+  );
+}
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,41 +1,30 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect } from "react";
+import CategoriesSlider from "./CategoriesSlider";
 
-export default function Landing({
-    getAllTypes,
-    types,
-}) {
-    
-    useEffect(() => {
-        getAllTypes();
-    }, [getAllTypes]);
+export default function Landing({ getAllTypes, types }) {
+  useEffect(() => {
+    getAllTypes();
+  }, [getAllTypes]);
 
-    // Hide NSFW categories from the landing page if the user is not logged in
-    const categories = types.filter((category) => !category.is_nsfw)
+  // Hide NSFW categories from the landing page if the user is not logged in
+  const categories = types.filter((category) => !category.is_nsfw);
 
-    return (
-        <div className='Landing'>
-            
-            <div className="Landing__introduction">
-                <h2 className="Landing__introduction__title">Acheter et Vendez vos mangas entre passionnés</h2>
-                <div className="Landing__introduction__post">
-                    Déposer une annonce
-                </div>
-            </div>
+  return (
+    <div className="Landing">
+      <div className="Landing__introduction">
+        <h2 className="Landing__introduction__title">
+          Acheter et Vendez vos mangas entre passionnés
+        </h2>
+        <div className="Landing__introduction__post">Déposer une annonce</div>
+      </div>
 
-            <div className="Landing__categories">
-                <h2 className="Landing__categories__title">Trouver votre prochain coup de coeur parmi les {categories.length} catégories disponibles</h2>
-                <ul className="Landing__categories__list">
-                    {categories.slice(0, 8).map((category, index) => (
-                        <li key={index} className="Landing__categories__list__item">
-                            <Link to={`/category/${category.label}`} className="Landing__categories__list__item__link">
-                                <img src={require(`../assets/images/categories/${category.label}.png`)} alt={category.name} />
-                                <span>{category.name}</span>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    )
+      <div className="Landing__categories">
+        <h2 className="Landing__categories__title">
+          Trouver votre prochain coup de coeur parmi les {categories.length}{" "}
+          catégories disponibles
+        </h2>
+        <CategoriesSlider categories={categories} />
+      </div>
+    </div>
+  );
 }
